7a82585dc0204f737340ac36351cc7a22e127327
Andres Arevalo <9005829@alumnos.ufv.es>
2022-10-21 12:47:23 +0200

    Completada la estructura del proyecto

--- src/com/pruebaCSV/LectorCSV.java
+++ src/com/pruebaCSV/LectorCSV.java
@@ -1,2 +1,24 @@
-package com.pruebaCSV;public class LectorCSV {
+package com.pruebaCSV;
+import java.io.FileReader;
+import com.opencsv.CSVReader;
+public class LectorCSV {
+    public void leeCSV(String nombreArchivo){
+        try{
+        CSVReader csvReader = new CSVReader(new FileReader(nombreArchivo));
+        String[] fila = null;
+        while ((fila = csvReader.readNext()) != null){
+            System.out.println((fila[0]
+                    + " | " + fila[1]
+                    + " | " + fila[2]));
+
+        }
+        csvReader.close();
+    }catch (Exception ex){
+        System.out.print(ex.getMessage());
+    }
+    }
 }
+
+
+
+
---
+++ src/com/pruebaCSV/PruebaCSV.java
@@ -0,0 +1,8 @@
+package com.pruebaCSV;
+
+public class PruebaCSV {
+    public static void main(String[] args){
+        LectorCSV miLector = new LectorCSV();
+        miLector.leeCSV("./datos/pokemon.csv");
+    }
+}
